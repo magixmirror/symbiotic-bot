f0e139af2107c3d2f269844a5949f3522e53b3da
magixmirror <goldmagixmirror@gmail.com>
2023-06-09 15:13:52 -0700

    SymbioticBot
    Our light chatbot using GPT 3.5 Turbo API. It's pretty simplistic. We plan to pair it with an image generator (as opposed to a chat log) and componentize them together and include them in our main site.

---
+++ mattGPT.js
@@ -0,0 +1,74 @@
+const url = 'https://api.openai.com/v1/chat/completions';
+
+const form = document.querySelector('form');
+const promptInput = document.querySelector('#prompt');
+const chatLog = document.querySelector('.chat-log');
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let value = promptInput.value;
+    if (value !== '') {
+       createMessageInstace(value);
+       askMattGPT(value);
+       handleScroll();
+       promptInput.value = ''; 
+    }
+})
+
+function createMessageInstace(prompt) {
+    chatLog.innerHTML +=
+    `
+    <div class="message-instance-container">
+        <div class="message user-message">
+            <div class="content">
+                <div class="message-image">
+                    <img src="./user.png" alt="User image">
+                </div>
+                <p style class="prompt">${prompt}</p>
+            </div>
+        </div>
+        <div class="message ai-message">
+            <div class="content">
+                <div class="message-image">
+                    <img src="./logo11.svg" alt="SymbioticBot logo">
+                </div>
+                <p class="thinking">Pondering</p>
+            </div>
+        </div>
+    </div>
+    `
+}
+
+function askMattGPT(prompt) {
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${process.env.CAPI_KEY}`
+        },
+        body: JSON.stringify({
+            model: 'gpt-3.5-turbo',
+            messages: [
+                {
+                    role: 'user',
+                    content: prompt
+                }
+            ],
+            max_tokens: 200,
+        })
+    })
+    .then(res => res.json())
+    .then (data => updateMessage(data))
+}
+
+// scrolls chatlog to bottom
+function handleScroll() {
+    chatLog.scrollTop = chatLog.scrollHeight;
+}
+
+function updateMessage(message) {
+    const p= document.querySelector('.thinking');
+    p.textContent = message.choices[0].message.content;
+    p.classList.remove('thinking');
+    handleScroll();
+}
